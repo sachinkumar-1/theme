db9d7a5ceff6627473b9e97b62c477d2950bb899
sachinkumar-1 <150107883+sachinkumar-1@users.noreply.github.com>
2023-12-21 13:11:37 +0530

    brand theme

---
+++ BrandTheme/Brandtheme.js
@@ -0,0 +1,484 @@
+import React from "react";
+import "./styles.css"; 
+
+const BrandsComponent = () => {
+  return (
+    <div className="container">
+      <div className="o-clients__row">
+        <div className="o-clients__col o-clients__col--left">
+          <div className="o-clients__items">
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2022/01/3.webp?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2021/09/Oyo-Hover.webp?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2022/01/6.webp?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2021/09/housing-hover-300x146-1-1.webp?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2023/09/tata1.webp?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2023/03/tata-removebg-preview.png?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2023/07/baidyanath.webp?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2023/07/baidyanath.webp?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2022/01/10.webp?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2022/01/Maruti-Suzuki.jpg?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2023/07/mankind.webp?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2023/07/mankind.webp?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2022/01/2-1.webp?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2021/09/Max-Case-Study_410x615-200x300-1.webp?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2023/07/carrier-midea.webp?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2023/07/carrier-midea.webp?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2023/07/crax.webp?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2023/07/crax.webp?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2022/01/8.webp?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2022/10/hp.webp?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2022/01/5.webp?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2021/09/Hitachi-Hover.webp?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2022/01/13.webp?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2021/09/campus-shoes.webp?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2023/03/download__8_-removebg-preview.png?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2023/03/download__8_-removebg-preview.png?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2023/03/download__9_-removebg-preview.png?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2023/03/download__9_-removebg-preview.png?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2022/01/4.webp?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2022/10/faces-canada.webp?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2023/03/download__2_-removebg-preview.png?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2023/03/download__2_-removebg-preview.png?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2022/01/18.webp?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2021/09/Jindal-Hover.jpg?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2022/01/15.webp?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2021/09/Marvel-Tea-Hover.jpg?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2022/01/20.webp?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2023/05/Shein-symbol-removebg-preview.png?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2023/05/Triveni-Engineering-Industries-Ltd.png?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2023/05/Triveni-Engineering-Industries-Ltd.png?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2023/09/tresmode2.webp?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2023/07/tresmode1.webp?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2022/01/7.webp?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2021/09/vivo.webp?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2022/01/12.webp?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2022/10/lkw-430x99999-1.webp?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+            <div className="o-clients__item">
+              <div itemScope="" itemType="https://schema.org/Brand">
+                <img
+                  itemProp="image"
+                  alt="brands"
+                  src="https://static.technians.com/wp-content/uploads/2022/01/1-1.webp?x67759"
+                  className="o-clients__logo"
+                  width={170}
+                  height={170}
+                />
+                <img
+                  src="https://static.technians.com/wp-content/uploads/2021/09/ananda-bg-image-1.webp?x67759"
+                  alt=""
+                  className="o-clients__image"
+                  width={207}
+                  height={207}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="read-more-wrapper d-block d-md-none">
+            <a
+              href="https://technians.com/our-clients/"
+              className="read-more-two smallerview"
+            >
+              <span className="read-more-icon" />
+              <span className="read-more-square" />
+              <span className="text">View more</span>
+            </a>
+          </div>
+        </div>
+      
+      </div>
+    </div>
+  );
+};
+
+export default BrandsComponent;
